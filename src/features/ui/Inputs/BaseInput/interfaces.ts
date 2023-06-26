86ae90595ba495328488ebc000986587ed144361
Narek <gnaro93@gmail.com>
2023-06-26 15:28:16 +0400

    Refactor

--- src/features/ui/Inputs/BaseInput/interfaces.ts
+++ src/features/ui/Inputs/BaseInput/interfaces.ts
@@ -1,6 +1,7 @@
 import { CSSProperties, ChangeEventHandler, RefObject, HTMLProps } from 'react'
 
 export interface IBaseInputProps extends Omit<HTMLProps<HTMLInputElement>, 'ref'> {
+  type: 'text' | 'password' | 'search' | 'url'
   autofocus?: boolean
   ref?: RefObject<HTMLInputElement>
   hasError?: boolean
