97861c8fb4d8abb0bda393f97f8382bd5716b155
Narek Grigoryan <gnaro93@gmail.com>
2024-04-03 13:59:05 +0400

    Deploy fix

--- src/features/ui/Buttons/BaseButton/interfaces.ts
+++ src/features/ui/Buttons/BaseButton/interfaces.ts
@@ -3,10 +3,10 @@ import { CSSProperties, HTMLProps, ReactNode, RefObject } from 'react'
 type SizeVariant = 'small' | 'medium' | 'large' | 'pagination'
 
 export interface IBaseButtonProps extends HTMLProps<HTMLButtonElement> {
-  fill?: 'outlined' | 'full-filled';
-  variant?: 'flat' | 'rounded';
-  sizeVariant?: SizeVariant;
-  children?: ReactNode;
-  style?: CSSProperties;
-  ref?: RefObject<HTMLButtonElement>;
+  fill?: 'outlined' | 'full-filled'
+  variant?: 'flat' | 'rounded'
+  sizeVariant?: SizeVariant
+  children?: ReactNode
+  style?: CSSProperties
+  ref?: RefObject<HTMLButtonElement>
 }
--- src/features/ui/Inputs/BaseInput/interfaces.ts
+++ src/features/ui/Inputs/BaseInput/interfaces.ts
@@ -1,6 +1,7 @@
-import { CSSProperties, ChangeEventHandler, RefObject, HTMLProps } from 'react'
+import { CSSProperties, ChangeEventHandler, HTMLProps, RefObject } from 'react'
 
-export interface IBaseInputProps extends Omit<HTMLProps<HTMLInputElement>, 'ref'> {
+export interface IBaseInputProps
+  extends Omit<HTMLProps<HTMLInputElement>, 'ref'> {
   onChange?: ChangeEventHandler<HTMLInputElement>
   type: 'text' | 'password' | 'search' | 'url'
   autofocus?: boolean
